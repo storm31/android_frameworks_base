ed4a7e534dc5d2f7460993b9ecc7b150d5dc4504
storm31 <sergej.michel@t-online.de>
2014-09-25 01:33:28 +0200

    - removed call of missing variable

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -319,14 +319,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 Settings.Global.getUriFor(Settings.Global.DEVICE_PROVISIONED), true,
                 mProvisioningObserver);
 
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.SHOW_NAVIGATION), true,
-                mShowNavObserver, UserHandle.USER_ALL);
-
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.LARGE_RECENT_THUMBS), true,
-                mShowNavObserver, UserHandle.USER_ALL);
-
         mSettingsObserver.observe();
 
         mBarService = IStatusBarService.Stub.asInterface(
